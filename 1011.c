75c4a00bfa48173c41c1d98f79d3c450f05aae9a
Safaat Akanda Fahim <140727330+safaat260@users.noreply.github.com>
2025-04-16 12:08:05 +0600

    Update 1011.c

--- 1011.c
+++ 1011.c
@@ -12,3 +12,41 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+#include <string.h>
+
+struct Object {
+    char name[10];
+    float weight;
+    float profit;
+    float pw_ratio;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    struct Object items[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s %f %f", items[i].name, &items[i].weight, &items[i].profit);
+        items[i].pw_ratio = items[i].profit / items[i].weight;
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (items[j].pw_ratio < items[j + 1].pw_ratio) {
+                items[j] = (struct Object){items[j + 1].name, items[j + 1].weight, items[j + 1].profit, items[j + 1].pw_ratio} +
+                           (items[j + 1] = items[j], 0);
+            }
+        }
+    }
+
+    printf("Name\tWeight\tProfit\tP/W\n");
+    for (int i = 0; i < n; i++) {
+        printf("%s\t%.2f\t%.2f\t%.2f\n", items[i].name, items[i].weight, items[i].profit, items[i].pw_ratio);
+    }
+
+    return 0;
+}
